d2f8c15b7b7096c67332388bf4c89dc52da49b5b
gaoyang5 <msy_gaoyang@163.com>
2016-12-27 16:03:53 +0800

    1

---
+++ JpushManager.h
@@ -0,0 +1,13 @@
+//
+//  JpushManager.h
+//  0900
+//
+//  Created by yang Gao on 16/12/27.
+//  Copyright © 2016年 yang Gao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JpushManager : NSObject
+
+@end
---
+++ JpushManager.m
@@ -0,0 +1,13 @@
+//
+//  JpushManager.m
+//  0900
+//
+//  Created by yang Gao on 16/12/27.
+//  Copyright © 2016年 yang Gao. All rights reserved.
+//
+
+#import "JpushManager.h"
+
+@implementation JpushManager
+
+@end
